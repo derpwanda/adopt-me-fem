30779c7707f0e319ced32139344347bf47c37c3f
Wanda Evans <derpwanda@gmail.com>
2022-03-02 13:49:13 -0600

    add handler, component working

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,6 +10,13 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  // add event listener
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index, //the '+' converts to num
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -20,8 +27,10 @@ class Carousel extends Component {
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
               src={photo}
+              data-index={index} //html thing, see handleIndexClick
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />

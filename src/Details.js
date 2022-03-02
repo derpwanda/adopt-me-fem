84b6e641c4f514ccba13852c95877c4478203807
Wanda Evans <derpwanda@gmail.com>
2022-03-02 14:07:27 -0600

    Error Boundaries

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 // you cannot use Hooks here
 
@@ -48,7 +49,11 @@ class Details extends Component {
 }
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;

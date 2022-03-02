9686e64d8199f8f3c538031c8311cd373ff8165f
Wanda Evans <derpwanda@gmail.com>
2022-03-02 11:04:13 -0600

    added Routing to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
+      <BrowserRouter>
         <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };

dd365bedd12e5a6f72f50130a86b248cff7d112d
Wanda Evans <derpwanda@gmail.com>
2022-03-02 10:28:37 -0600

    wrap app in  StrictMode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  )
-}
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
 
-{/* <Pet name="Princess" animal="dog" breed="Labrador Retriever"/>
+{
+  /* <Pet name="Princess" animal="dog" breed="Labrador Retriever"/>
 <Pet name="Christopher" animal="Rodent" breed="Hamster"/>
-<Pet name="Quick" animal="Cat" breed="Mixed Breed"/> */}
+<Pet name="Quick" animal="Cat" breed="Mixed Breed"/> */
+}

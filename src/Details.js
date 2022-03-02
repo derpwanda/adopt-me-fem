0538e20bad5e1692968c0db29c091f3c2665dd0e
Wanda Evans <derpwanda@gmail.com>
2022-03-02 13:36:48 -0600

    create photo carousel, add to details, not working

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
+import Carousel from "./Carousel";
 
 // you cannot use Hooks here
 
@@ -29,10 +30,12 @@ class Details extends Component {
       return <h2>loading ...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>

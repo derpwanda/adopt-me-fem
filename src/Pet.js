77a92387bac519e4dab1bf97968e9c9b720cacbf
Wanda Evans <derpwanda@gmail.com>
2022-03-02 10:14:48 -0600

    add styling to Pet

--- src/Pet.js
+++ src/Pet.js
@@ -1,21 +1,32 @@
 // import React from "react";
 // JSX
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  )
-}
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pet-images.dev-apis.com/pets/none.jpg";
 
-{/* export default function Pet({ name, animal, breed }) {
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
+{
+  /* export default function Pet({ name, animal, breed }) {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed),
   ]);
-} */}
+} */
+}
 
 export default Pet;

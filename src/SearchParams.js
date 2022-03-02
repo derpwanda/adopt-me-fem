6819524feaaf3f2e51b9ecc8941444916c1888e2
Wanda Evans <derpwanda@gmail.com>
2022-03-02 10:13:59 -0600

    create Results component, add to Search

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile", "rodent"];
 
@@ -84,14 +85,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
